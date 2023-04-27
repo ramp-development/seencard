dd192b616fdb2b0cdcf79bd67143947de71b5cbc
g4knr <85592362+g4knr@users.noreply.github.com>
2023-04-27 14:37:42 +0100

    Updated animations

--- src/animations/nav.ts
+++ src/animations/nav.ts
@@ -7,25 +7,40 @@ export const nav = () => {
   const navComponent = navPos.querySelector('.nav_component');
   if (!navComponent) return;
 
+  const navBanner = navPos.querySelector('.nav_banner');
+
   let previousScrollY = window.scrollY;
   const { innerHeight } = window;
+
+  if (previousScrollY > innerHeight) scrolledNav();
+
   window.addEventListener('scroll', () => {
     if (previousScrollY < innerHeight && window.scrollY > innerHeight) {
-      gsap.set(navPos, { position: 'fixed', translateY: '-100%', duration: 0 });
-      navComponent.classList.add('is-scrolled');
-      gsap.to(navPos, { translateY: 0, duration: 0.5, ease: 'power2.out' });
+      scrolledNav();
     } else if (previousScrollY > innerHeight && window.scrollY < innerHeight) {
-      gsap.to(navPos, {
-        translateY: '-100%',
-        duration: 0.5,
-        ease: 'power2.out',
-        onComplete: () => {
-          navComponent.classList.remove('is-scrolled');
-          gsap.set(navPos, { position: 'absolute', translateY: 0, duration: 0 });
-        },
-      });
+      staticNav();
     }
 
     previousScrollY = window.scrollY;
   });
+
+  function scrolledNav() {
+    gsap.set(navPos, { position: 'fixed', translateY: '-100%', duration: 0 });
+    navComponent.classList.add('is-scrolled');
+    navBanner.style.display = 'none';
+    gsap.to(navPos, { translateY: 0, duration: 0.5, ease: 'power2.out' });
+  }
+
+  function staticNav() {
+    gsap.to(navPos, {
+      translateY: '-100%',
+      duration: 0.5,
+      ease: 'power2.out',
+      onComplete: () => {
+        navComponent.classList.remove('is-scrolled');
+        navBanner.style.removeProperty('display');
+        gsap.set(navPos, { position: 'absolute', translateY: 0, duration: 0 });
+      },
+    });
+  }
 };
