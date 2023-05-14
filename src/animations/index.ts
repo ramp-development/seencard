44bdafe6b5cf36c4a5bc37eba2af860eec19e99d
g4knr <85592362+g4knr@users.noreply.github.com>
2023-05-15 00:14:05 +0100

    Testing scroll smoother

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,9 +1,11 @@
 import { groups } from './groups';
 import { highlight } from './highlight';
 import { nav } from './nav';
+import { scroll } from './scroll';
 import { tabs } from './tabs';
 
 export const animations = () => {
+  scroll();
   tabs();
   highlight();
   groups();
