20d8f0e1b221f317a3c45adfacfe3c41b9a105fe
g4knr <85592362+g4knr@users.noreply.github.com>
2023-05-16 17:18:45 +0100

    Further Safari bugs

--- src/animations/nav.ts
+++ src/animations/nav.ts
@@ -1,4 +1,5 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export const nav = () => {
   const navPos = document.querySelector('.nav_pos');
@@ -9,30 +10,19 @@ export const nav = () => {
 
   // define gsap timeline
   const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: document.body,
+      start: `top -${window.innerHeight}`,
+      onEnter: () => timeline.play(),
+      onLeaveBack: () => timeline.reverse(),
+    },
     paused: true,
     onStart: () => navComponent.classList.add('is-scrolled'),
     onReverseComplete: () => navComponent.classList.remove('is-scrolled'),
   });
+
   timeline
     .set(navBanner, { display: 'none' })
     .set(navPos, { position: 'fixed', translateY: '-100%', duration: 0 })
     .to(navPos, { translateY: 0, duration: 0.5, ease: 'power2.out' });
-
-  // define the scroll position and window height
-  let previousScrollY = window.scrollY;
-  const { innerHeight } = window;
-
-  // play the timeline if the user has scrolled past the window height
-  if (previousScrollY > innerHeight) timeline.play();
-
-  // listen to the scroll event and play or reverse the timeline
-  window.addEventListener('scroll', () => {
-    if (previousScrollY < innerHeight && window.scrollY > innerHeight) {
-      timeline.play();
-    } else if (previousScrollY > innerHeight && window.scrollY < innerHeight) {
-      timeline.reverse();
-    }
-
-    previousScrollY = window.scrollY;
-  });
 };
