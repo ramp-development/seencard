2a6697fedf7bcf6e2b62ab1aac47bf9cfdf1a5ac
g4knr <85592362+g4knr@users.noreply.github.com>
2023-09-27 17:29:53 +0100

    log browser

--- src/animations/utils/broswer.ts
+++ src/animations/utils/broswer.ts
@@ -2,6 +2,8 @@ import { detectBrowser } from '$utils/detectBroswer';
 export const browser = (): boolean => {
   const browser = detectBrowser();
 
+  console.log(browser);
+
   if (browser.name !== 'Safari') return true;
   if (Number(browser.shortVersion) >= 16) return true;
   return false;
